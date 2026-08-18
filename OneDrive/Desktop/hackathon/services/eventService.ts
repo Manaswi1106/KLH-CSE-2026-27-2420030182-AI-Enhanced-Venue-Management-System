1568f1c5187b0f28f0fbe27e5f25d883e9c83cf8
Manaswi1106 <nagamanaswiganji@gmail.com>
2026-08-18 12:16:27 +0530

    Delete OneDrive/Desktop/hackathon directory

--- OneDrive/Desktop/hackathon/services/eventService.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "./firebase";
-import { addDoc, collection } from "firebase/firestore";
-
-export const createEvent = async (event: any) => {
-  return await addDoc(collection(db, "events"), event);
-};
